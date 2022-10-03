cb68e300d512db85357eb5b697251910cc192c96
vguenoff <vguenoff@gmail.com>
2022-10-03 15:20:43 +0300

    Visualise results

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 import './style.scss'
 
 const searchInput = document.getElementById('searchInput')
-
-const showData = []
+const movies = document.querySelector('.movies')
 
 const upperCaseRobot = sentence =>
     sentence
@@ -17,27 +16,39 @@ async function fetchShowData(name) {
         const res = await fetch(`https://api.tvmaze.com/search/shows?q=${name}`)
         const data = await res.json()
 
-        showData.push(
-            ...data
-                .filter(({ show }) => !show.name.toLowerCase().includes('show'))
-                .map(data => {
-                    const { show } = data
-                    const updatedShow = {
-                        ...show,
-                        name: upperCaseRobot(show.name),
-                        summary: upperCaseRobot(show.summary),
-                    }
+        return data
+            .filter(({ show }) => !show.name.toLowerCase().includes('show'))
+            .map(data => {
+                const { show } = data
+                const updatedShow = {
+                    ...show,
+                    name: upperCaseRobot(show.name),
+                    summary: upperCaseRobot(show.summary),
+                }
 
-                    return {
-                        data,
-                        ...updatedShow,
-                    }
-                }),
-        )
+                return {
+                    data,
+                    ...updatedShow,
+                }
+            })
     } catch (error) {
         console.error(error)
     }
 }
 
-await fetchShowData('robot')
-console.log({ showData })
+async function displayMatches() {
+    const showData = await fetchShowData(this.value)
+
+    movies.innerHTML = showData
+        ?.map(
+            show =>
+                `
+                <li>
+                    <span class="population">${show.name}</span>
+                </li>
+            `,
+        )
+        .join('')
+}
+
+searchInput.addEventListener('keyup', displayMatches)
